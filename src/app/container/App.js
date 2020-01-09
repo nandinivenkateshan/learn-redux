de222b2bfd4e4bd22b6b373c375b4536fdbfb38c
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2020-01-09 09:11:28 +0530

    Added action files

--- src/app/container/App.js
+++ src/app/container/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Main from '../components/Main'
 import User from '../components/User'
 import { connect } from 'react-redux'
+import { setName } from '../actions/userActions'
 
 class App extends React.Component {
   render () {
@@ -24,10 +25,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     setName: (name) => {
-      dispatch({
-        type: 'SET_NAME',
-        payload: name
-      })
+      dispatch(setName(name))
     }
   }
 }
